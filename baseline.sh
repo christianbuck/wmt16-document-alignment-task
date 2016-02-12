c9154dd6c99c5b7954e019d6c91749f60dcaee2c
Christrian Buck <cbuck@lantis.de>
2016-02-12 13:15:07 +0100

    Proper linebreaks

--- baseline.sh
+++ baseline.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 zcat lett.train/*.lett.gz | cut -f 1,4 | python match_url_pairs.py  -pairs predicted.pairs -devset train.pairs
-echo '\n\nRunning eval\n\n'
+echo -e '\nRunning eval\n'
 python eval.py train.pairs predicted.pairs
